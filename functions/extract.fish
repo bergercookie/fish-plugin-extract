3c0900dccfb2c7de316751e214bb838006f7fde7
Nikos Koukis <nickkouk@gmail.com>
2020-03-15 13:43:14 +0000

    Remove x, check arguments before exec

--- functions/extract.fish
+++ functions/extract.fish
@@ -1,31 +1,38 @@
 # Taken from: https://github.com/dideler/dotfiles/blob/master/functions/extract.fish
 
-function extract --description "Expand or extract bundled & compressed files"
-  set --local ext (echo $argv[1] | awk -F. '{print $NF}')
+function extract --description "Expand or extract bundled & compressed files" --argument archive
+  if test -n "$archive" # not empty
+    set archive_ $archive
+  else
+    echo "Usage: " (status --current-filename) "<archive-to-uncompress>"
+    return 1
+  end
+
+  set --local ext (echo $archive_ | awk -F. '{print $NF}')
   switch $ext
     case tar  # non-compressed, just bundled
-      tar -xvf $argv[1]
+      tar -xvf $archive_
     case gz
-      if test (echo $argv[1] | awk -F. '{print $(NF-1)}') = tar  # tar bundle compressed with gzip
-        tar -zxvf $argv[1]
+      if test (echo $archive_ | awk -F. '{print $(NF-1)}') = tar  # tar bundle compressed with gzip
+        tar -zxvf $archive_
       else  # single gzip
-        gunzip $argv[1]
+        gunzip $archive_
       end
     case tgz  # same as tar.gz
-      tar -zxvf $argv[1]
+      tar -zxvf $archive_
     case bz2  # tar compressed with bzip2
-      tar -jxvf $argv[1]
+      tar -jxvf $archive_
     case rar
-      unrar x $argv[1]
+      unrar x $archive_
     case zip
-      unzip $argv[1]
+      unzip $archive_
     case xz
-      if test (echo $argv[1] | awk -F. '{print $(NF-1)}') = tar  # tar bundle compressed with xz
-        tar -Jxf $argv[1]
+      if test (echo $archive_ | awk -F. '{print $(NF-1)}') = tar  # tar bundle compressed with xz
+        tar -Jxf $archive_
       else  # single xz
-        unxz $argv[1]
+        unxz $archive_
       end
     case '*'
-      echo "unknown extension"
+      echo "Unknown extension [$ext], bailing out"
   end
 end
--- functions/x.fish
+++
@@ -1,3 +0,0 @@
-function x --description "alias extract as x"
-  extract $argv
-end
